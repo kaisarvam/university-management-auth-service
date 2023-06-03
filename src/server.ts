3bf6171b3296f393d392689b798bacc699e3e1f0
kaisar Sarwar <kaisarvam3@gmail.com>
2023-06-03 18:45:09 +0600

    [UAS-001] setup express,mongoose,cors,parser etc

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import config from "./config";
+import app from "./app";
+const port = 3000;
+
+async function bootstrap() {
+    try {
+        await mongoose.connect(config.databaseUrl as string);
+        console.log('Database Connected successfully!!!');
+        app.listen(3000, () => {
+            console.log(`Server is running at http://localhost:${config.port}`);
+            
+        })
+    } catch (error) {
+        console.log('Connect failure!!! ',error);
+    }
+}
+
+bootstrap();
