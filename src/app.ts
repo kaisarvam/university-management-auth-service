6d743849905807cb704bafb988336a6993cc5c3b
kaisar Sarwar <kaisarvam3@gmail.com>
2023-06-04 12:02:28 +0600

    husky test4

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 const app: Application = express()
 
 app.use(cors())
-        app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
