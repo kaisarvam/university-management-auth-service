9b1570c18d481a3e7d2a85d62c9eeca33fe2d564
kaisar Sarwar <kaisarvam3@gmail.com>
2023-06-19 21:13:08 +0600

    code format

--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -1,8 +1,15 @@
-import { Schema } from 'mongoose'
+import { Schema, model } from 'mongoose'
 import { IUser } from './user.interface'
 
-export const UserSchema = new Schema<IUser>({
-  id: { type: String, required: true, unique: true },
-  role: { type: String, required: true },
-  password: { type: String, required: true },
-})
+export const UserSchema = new Schema<IUser>(
+  {
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export const UserModel = model<IUser>('User', UserSchema)
